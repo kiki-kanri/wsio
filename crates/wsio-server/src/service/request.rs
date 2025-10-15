1ce50567a2c71fb2b29eae5e42de09e541da402a
kiki-kanri <a470666@gmail.com>
2025-10-15 15:30:37 +0800

    feat: enhance `dispatch_request` with header validation and WebSocket support
    - added more header checks
    - improved final response headers
    - implemented WebSocket accept key calculation

--- crates/wsio-server/src/service/request.rs
+++ crates/wsio-server/src/service/request.rs
@@ -1,34 +1,56 @@
 use std::sync::Arc;
 
 use http::{
+    HeaderName,
     Method,
     Request,
     Response,
     StatusCode,
     header,
 };
+use hyper::upgrade::OnUpgrade;
+use tungstenite::handshake::derive_accept_key;
 use url::form_urlencoded;
 
 use crate::runtime::WsIoRuntime;
 
+#[inline]
+fn check_header_value<ReqBody>(request: &Request<ReqBody>, name: HeaderName, expected_value: &str) -> bool {
+    get_header_value(request, name)
+        .map(|v| v.eq_ignore_ascii_case(expected_value))
+        .unwrap_or(false)
+}
+
 pub async fn dispatch_request<ReqBody, ResBody: Default, E: Send>(
-    request: Request<ReqBody>,
+    mut request: Request<ReqBody>,
     runtime: Arc<WsIoRuntime>,
 ) -> Result<Response<ResBody>, E> {
+    // Check method
     if request.method() != Method::GET {
         return respond(StatusCode::METHOD_NOT_ALLOWED);
     }
 
-    if !request
-        .headers()
-        .get(header::UPGRADE)
-        .and_then(|h| h.to_str().ok())
-        .map(|v| v.eq_ignore_ascii_case("websocket"))
-        .unwrap_or(false)
-    {
+    // Check upgrade header
+    if !check_header_value(&request, header::UPGRADE, "websocket") {
         return respond(StatusCode::BAD_REQUEST);
     }
 
+    // Check connection header
+    if !check_header_value(&request, header::CONNECTION, "upgrade") {
+        return respond(StatusCode::BAD_REQUEST);
+    }
+
+    // Check websocket version header
+    if !check_header_value(&request, header::SEC_WEBSOCKET_VERSION, "13") {
+        return respond(StatusCode::BAD_REQUEST);
+    }
+
+    // Get websocket sec key
+    let Some(websocket_sec_key) = get_header_value(&request, header::SEC_WEBSOCKET_KEY) else {
+        return respond(StatusCode::BAD_REQUEST);
+    };
+
+    // Get namespace path
     let Some(namespace_path) = request.uri().query().and_then(|q| {
         form_urlencoded::parse(q.as_bytes())
             .find(|(k, _)| k == "namespace")
@@ -37,11 +59,32 @@ pub async fn dispatch_request<ReqBody, ResBody: Default, E: Send>(
         return respond(StatusCode::BAD_REQUEST);
     };
 
+    // Get namespace
     let Some(namespace) = runtime.get_namespace(&namespace_path) else {
         return respond(StatusCode::NOT_FOUND);
     };
 
-    respond(StatusCode::SWITCHING_PROTOCOLS)
+    // Generate accept key
+    let websocket_accept_key = derive_accept_key(websocket_sec_key.as_bytes());
+
+    // Upgrade
+    let on_upgrade = match request.extensions_mut().remove::<OnUpgrade>() {
+        Some(upgrade) => upgrade,
+        None => return respond(StatusCode::INTERNAL_SERVER_ERROR),
+    };
+
+    Ok(Response::builder()
+        .status(StatusCode::SWITCHING_PROTOCOLS)
+        .header(header::CONNECTION, "Upgrade")
+        .header(header::SEC_WEBSOCKET_ACCEPT, websocket_accept_key)
+        .header(header::UPGRADE, "websocket")
+        .body(ResBody::default())
+        .unwrap())
+}
+
+#[inline]
+fn get_header_value<ReqBody>(request: &Request<ReqBody>, name: HeaderName) -> Option<&str> {
+    request.headers().get(name).and_then(|v| v.to_str().ok())
 }
 
 #[inline]
