49d7440ee1c830d29583029c21baff9bbfb46e5d
kiki-kanri <a470666@gmail.com>
2025-10-18 12:53:33 +0800

    feat(server): implement `with_auth` method in namespace builder

--- crates/wsioxide-server/src/namespace/builder.rs
+++ crates/wsioxide-server/src/namespace/builder.rs
@@ -1,6 +1,10 @@
-use std::sync::Arc;
+use std::{
+    pin::Pin,
+    sync::Arc,
+};
 
 use anyhow::Result;
+use serde::de::DeserializeOwned;
 
 use super::{
     WsIoServerNamespace,
@@ -38,6 +42,26 @@ impl WsIoServerNamespaceBuilder {
         let namespace = Arc::new(WsIoServerNamespace::new(self.config, self.runtime.clone()));
         self.runtime.insert_namespace(namespace.clone())?;
         Ok(namespace)
+    }
+
+    pub fn with_auth<H, Fut, A: DeserializeOwned>(mut self, handler: H) -> WsIoServerNamespaceBuilder
+    where
+        H: Fn(Arc<WsIoServerConnection>, A) -> Fut + Send + Sync + 'static,
+        Fut: Future<Output = Result<()>> + Send + 'static,
+    {
+        let handler = Arc::new(handler);
+        let packet_codec = self.config.packet_codec.clone();
+        let arc_handler = Arc::new(move |connection, bytes: Vec<u8>| {
+            let handler = handler.clone();
+            let packet_codec = packet_codec.clone();
+            Box::pin(async move {
+                let auth_data = packet_codec.decode_data::<A>(&bytes)?;
+                handler(connection, auth_data).await
+            }) as Pin<Box<dyn Future<Output = Result<()>> + Send + 'static>>
+        });
+
+        self.config.auth_handler = Some(arc_handler);
+        self
     }
 
     pub fn with_packet_codec(mut self, packet_codec: WsIoPacketCodec) -> WsIoServerNamespaceBuilder {
--- crates/wsioxide-server/src/namespace/config.rs
+++ crates/wsioxide-server/src/namespace/config.rs
@@ -1,10 +1,25 @@
+use std::{
+    pin::Pin,
+    sync::Arc,
+};
+
+use anyhow::Result;
+
 use crate::{
+    connection::WsIoServerConnection,
     core::packet::codecs::WsIoPacketCodec,
     types::handler::WsIoServerConnectionOnConnectHandler,
 };
 
 pub(super) struct WsIoServerNamespaceConfig {
-    pub(super) auth_handler: Option<()>,
+    pub(super) auth_handler: Option<
+        Arc<
+            dyn Fn(Arc<WsIoServerConnection>, Vec<u8>) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'static>>
+                + Send
+                + Sync
+                + 'static,
+        >,
+    >,
     pub(super) on_connect_handler: WsIoServerConnectionOnConnectHandler,
     pub(super) packet_codec: WsIoPacketCodec,
     pub(super) path: String,
