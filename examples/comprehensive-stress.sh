11df267dcdf5d456e165dfb6e580c1a84bf43379
kiki-kanri <a470666@gmail.com>
2025-10-27 23:04:16 +0800

    examples: add some stress files

---
+++ examples/comprehensive-stress.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPTS_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+cd "${SCRIPTS_DIR}"
+
+cd client/
+cargo r --bin comprehensive-stress --all-features "${@}"
---
+++ examples/stress-server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPTS_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+cd "${SCRIPTS_DIR}"
+
+cd server/
+cargo r --bin stress --all-features "${@}"
